38e01c3beb68f934e9c910fe5aa936de6a8b6360
CJM1994 <connorjohnmullin@gmail.com>
2021-11-29 21:32:11 -0800

    Added Basic UI

--- script.js
+++ script.js
@@ -12,6 +12,9 @@
 // Rock == 0, Paper == 1, Scissors == 2
 let playerChoice = 0;
 let compChoice = 0;
+let playerPoints = 0;
+let computerPoints = 0;
+let tiePoints = 0;
 
 function compChoose() {
     // Selects a random number for the computer player 0-2, 0 == rock, 1 == paper, 2 == scissors
@@ -19,9 +22,10 @@ function compChoose() {
     return compChoice;
 }
 
-function playerChoose() {
+function playerChoose(e) {
     // Collects the player choice as a text string and returns 0 for rock, 1 for paper, 2 for scissors. Function will repeat if invalid answer.
-    playerChoice = prompt('Enter your choice: Rock, Paper, Scissors').toLowerCase();
+
+    playerChoice = e.srcElement.className;
 
     switch (playerChoice) {
         case 'rock': playerChoice = 0;
@@ -33,82 +37,74 @@ function playerChoose() {
         case 'scissors': playerChoice = 2;
             return playerChoice;
 
-        default: alert('Invalid entry, please choose again!'); playerChoose();
-            break;
+        default: playerChoice = 'error';
+            return playerChoice; // Handles error if clicking something other than button
     }
 }
 
-function playRound() {
+function playRound(e) {
 
     compChoose();
-    playerChoose();
+    playerChoose(e);
+
+    if (playerChoice === 'error') return;
 
     // Runs if player and computer pick the same choice
     if (playerChoice === compChoice) {
-        return ('tie');
+        updatePoints('tie');
+        return;
     }
     // Runs if player wins with scissors vs paper, or with paper vs rock
     else if (playerChoice - compChoice === 1) {
-        return ('playerWins');
+        updatePoints('playerWins');
+        return;
     }
     // Runs if computer wins with scissors vs paper, or with paper vs rock
     else if (compChoice - playerChoice === 1) {
-        return ('computerWins');
+        updatePoints('computerWins');
+        return;
     }
     // Runs if player wins with rock vs scissors
     else if (playerChoice - compChoice === -2) {
-        return ('playerWins');
+        updatePoints('playerWins');
+        return;
     }
     // Runs if computer wins with rock vs scissors
     else if (compChoice - playerChoice === -2) {
-        return ('computerWins');
+        updatePoints('computerWins');
+        return;
     }
     // This should not happen
-    else {
-        return ('error');
-    }
-
+    else return;
 }
 
-function playGame() {
-
-    let playerPoints = 0; // Tracks player's points in a 5 round game
-    let computerPoints = 0; // Tracks computer's points in a 5 round game
-    let result = ''; // Stores result of each round to determine who to give points to
-
-    for (let i = 0; i < 5; i++) {
-
-        result = playRound();
-
-        if (result === 'playerWins') {
-            playerPoints++;
-            console.log('Player Wins Round!');
-            console.log('Your score is ' + playerPoints + '!');
-            console.log('Your opponents score is ' + computerPoints + '!');
-        }
-        else if (result === 'computerWins') {
-            computerPoints++;
-            console.log('Computer Wins Round!');
-            console.log('Your score is ' + playerPoints + '!');
-            console.log('Your opponents score is ' + computerPoints + '!');
-        }
-        else {
-            console.log('Tie Round!');
-            console.log('Your score is ' + playerPoints + '!');
-            console.log('Your opponents score is ' + computerPoints + '!');
-        }
+function updatePoints(roundResult) {
+    if (roundResult === 'playerWins') {
+        playerPoints++;
+        playerScore.textContent = `Player: ${playerPoints}`;
     }
-
-    if (playerPoints > computerPoints) {
-        alert('CONGRATULATIONS! YOU ARE A WINNER!');
+    else if (roundResult === 'computerWins') {
+        computerPoints++;
+        computerScore.textContent = `Computer: ${computerPoints}`
     }
-    else if (computerPoints > playerPoints) {
-        alert('TOO BAD! YOU ARE A LOSER!');
+    else if (roundResult === 'tie') {
+        tiePoints++;
+        tieScore.textContent = `Ties: ${computerPoints}`
     }
-    else {
-        alert('you tied... but at least you tried...')
+    if (playerPoints === 5) {
+        alert('YOU WON!!!')
     }
-
+    else if (computerPoints === 5) {
+        alert('YOU LOST!!!')
+    }
 }
 
-playGame();
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => addEventListener('click', playRound));
+
+const playerScore = document.querySelector('.player-score');
+playerScore.textContent = `Player: ${playerPoints}`;
+const computerScore = document.querySelector('.computer-score');
+computerScore.textContent = `Computer: ${computerPoints}`;
+const tieScore = document.querySelector('.tie-score');
+tieScore.textContent = `Ties: ${tiePoints}`;
